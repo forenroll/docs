babac12c5cf39c3d8f82c809b83f09e2cfe2e04e
xmeng <xmeng@localhost.localdomain>
2014-10-08 03:39:55 -0400

    g

--- labsapp/old/config/initializers/labs.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.before_initialize do
-  $lab_id = 'labsapp'
-  $lab_name = 'Labs App'
-  $lab_context = "/labs/#{$lab_id}"
-  $is_prod_gear = ENV['HOME'].eql? "labsprod"
-  Rails.application.config.assets.prefix = "#{$lab_context}/assets"
-  Rails.application.config.action_controller.relative_url_root = $lab_context
-  ENV['RAILS_RELATIVE_URL_ROOT'] = $lab_context
-end
