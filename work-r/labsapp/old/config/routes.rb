a9c5c407222a26f53ca7bdde4dbca76d289c2a70
xmeng <xmeng@localhost.localdomain>
2014-10-08 04:11:39 -0400

    g

--- work-r/labsapp/old/config/routes.rb
+++
@@ -1,7 +0,0 @@
-RailsApp::Application.routes.draw do
-  scope $lab_context do
-    # to get the lab_context
-    require Rails.root.join('config', 'initializers', 'labs.rb')
-    get "/", to: 'application#index'
-  end
-end
